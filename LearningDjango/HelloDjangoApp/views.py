1be39d4529f509e6b28d22688f78f3f8a04682cb
Abdur Rehman Shahid <as315@hw.ac.uk>
2019-07-12 01:36:34 +1000

    second

--- LearningDjango/HelloDjangoApp/views.py
+++ LearningDjango/HelloDjangoApp/views.py
@@ -2,8 +2,29 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from datetime import datetime
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    #html_content = "<html><head><title>Hello, Django</title></head><body>"
+    #html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+    #html_content += "</body></html>"
+
+    return render(
+        request,
+        "HelloDjangoApp/index.html",
+        {
+            'content': "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X"),
+            
+        }
+    )
 def app(request):
-    return HttpResponse("Hello, Django the view that I made!")
+    a=5
+    return render(
+        request,
+        "HelloDjangoApp/app.html",
+        {
+            'ColumnName': "Total Impressions",
+            'value':a
+        }
+    )
